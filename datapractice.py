b9f576ea2327d7347c85fc9310d3541437eb3b50
RosTobic <54326493+RosTobic@users.noreply.github.com>
2019-10-24 09:46:46 -0700

    Create datapractice.py

---
+++ datapractice.py
@@ -0,0 +1,8 @@
+dict={'Jan':'1', 'Feb':'2', 'Mar':'3', 'Apr':'4', 'May':'5', 'Jun':'6', 'Jul':'7', 'Aug':'8', 'Sep':'9', 'Oct':'10', 'Nov':'11', 'Dec':'12'}
+#print(dict['Dec'])
+for month in dict:
+    if month.startswith('M'):
+        print(month)
+  
+  AidanDict={'name':'Aidan', 'age':16, 'classes'={'1st': 'Math', '2nd':'DSW', '3rd':'marinbio', '4th':'English'}}
+  nicDict={'name':'Nic', 'age':16, 'classes'={'1st': 'Math', '2nd':'DSW', '3rd':'marinbio', '4th':'English'}}
